78b49b0fee5571958a8437cff33a1b54a8bd094b
joelmzramos <joel.ramos@accenture.com>
2019-08-28 22:31:24 -0300

    primeira versão

--- starter-code/app.js
+++ starter-code/app.js
@@ -8,10 +8,11 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
-
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 
 mongoose
-  .connect('mongodb://localhost/starter-code', {useNewUrlParser: true})
+  .connect('mongodb://localhost/lab-express-basic-auth', { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -29,7 +30,14 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use(session({
+  secret: 'basic-auth-secret',
+  cookie: { maxAge: 60000000 },
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60, // 1 day
+  }),
+}));
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,74 @@
 const express = require('express');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
 const router  = express.Router();
+const saltRounds = 10;
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/signup', (req, res) => {
+  res.render('signup');
+})
+
+router.post('/signup', async (req, res) => {
+  const { username, password } = req.body;
+
+  if(username === '' || password === '') {
+    res.render('signup', { errorMessage: 'Usuário ou Senha não preenchidos!' });
+    return;
+  }
+
+  const user = await User.findOne({ username });
+  if(user) {
+    res.render('signup', { errorMessage: 'Usuário já cadastrado!' });  
+    return;  
+  }
+
+  const salt = bcrypt.genSaltSync(saltRounds);
+  const hash = bcrypt.hashSync(password, salt);
+
+  const newUser = new User({ username, password: hash });
+  try {    
+    await newUser.save();
+    // res.redirect('/')
+  } catch (error) {
+    console.log(error);
+  }
+
+
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+})
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  if(username === '' || password === '') {
+    res.render('login', { errorMessage: 'Usuário ou Senha não preenchidos!' });
+    return;
+  }
+
+  const user = await User.findOne({ username });
+  if(!user) {
+    res.render('login', { errorMessage: 'Usuário não cadastrado!' });  
+    return;  
+  }
+  if(bcrypt.compareSync(password, user.password)) {
+    req.session.currentUser = user;
+    console.log('logei');
+    // res.redirect('')
+  } else {
+    res.render('login', { errorMessage: 'Usuário ou senha inválidos!' });  
+    return; 
+  }
+
+
 });
 
 module.exports = router;
